052f587ac958dd9274fe636cbf36a3ac4e137d88
constElfu <103916629+constelfu@users.noreply.github.com>
2022-05-16 15:42:50 +0900

    220516-last-modified
    220516 vanillaJS challenge 최종 수정

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const inputMax = document.querySelector("#inputMax");
 
 const inputForm = document.querySelector("#inputNum");
-const inputValue = document.querySelector("#inputNum input")
+const inputValue = document.querySelector("#inputValue");
 
 function inputSubmitted(event) {
-  const random = Math.round(Math.random() * inputMax.value);
   event.preventDefault();
 
-  const numText = document.querySelector("#submittedNum")
-  const checkText = document.querySelector("#check")
-  numText.innerText = `선택한 숫자: ${inputValue.value}, 랜덤생성된 숫자: ${random}`;
-  if (parseInt(inputValue.value) === random) {
-    checkText.innerText = "You win!";
+  const numText = document.querySelector("#submittedNum");
+  const checkText = document.querySelector("#check");
+
+  const parsedMaxValue = parseInt(inputMax.value);
+  const parsedNumValue = parseInt(inputValue.value);
+
+  const random = Math.round(Math.random() * parsedMaxValue);
+  numText.innerText = `선택한 숫자: ${parsedNumValue}, 랜덤생성된 숫자: ${random}`;
+
+  if (parsedMaxValue > parsedNumValue ) {
+    if (parsedNumValue === random) {
+      checkText.innerText = "You won!";
+    } else {
+      checkText.innerText = "You lost!";
+    }
   } else {
-    checkText.innerText = "You lose!";
+    alert("입력한 숫자 범위 내에서 숫자를 선택해야 합니다!");
+    checkText.innerText = "";
+    numText.innerText = "";
   }
+  
 }
 
 inputForm.addEventListener("submit", inputSubmitted);
