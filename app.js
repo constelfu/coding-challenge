41000a5158ef19428beab1c5319b675cc7684147
constElfu <103916629+constelfu@users.noreply.github.com>
2022-05-18 15:03:51 +0900

    vanillaJS-challenge-dDay
    디데이 계산기

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
-const inputMax = document.querySelector("#inputMax");
+const dDayClock = document.querySelector("#clock-here")
 
-const inputForm = document.querySelector("#inputNum");
-const inputValue = document.querySelector("#inputValue");
-
-function inputSubmitted(event) {
-  event.preventDefault();
-
-  const numText = document.querySelector("#submittedNum");
-  const checkText = document.querySelector("#check");
-  const alertText = document.querySelector("#alert");
-
-  const parsedMaxValue = parseInt(inputMax.value);
-  const parsedNumValue = parseInt(inputValue.value);
-  const random = Math.round(Math.random() * parsedMaxValue);
+function remainTime() {
+  const today = new Date();
+  const dDay = new Date("2022-12-25T00:00:00+0900");
+  const until = dDay.getTime() - today.getTime();
   
-  if (parsedMaxValue >= parsedNumValue ) {
-    numText.innerText = `선택한 숫자: ${parsedNumValue}, 랜덤생성된 숫자: ${random}`;
-    alertText.innerText = "";
-    if (parsedNumValue === random) {
-      checkText.innerText = "You won!";
-    } else {
-      checkText.innerText = "You lost!";
-    }
-  } else {
-    alertText.innerText = "입력한 숫자의 범위 내에서 숫자를 선택해야 합니다!"
-    checkText.innerText = "";
-    numText.innerText = "";
-  }
+  const remainDays =    String(Math.floor(until / (1000 * 60 * 60 * 24))).padStart(3,"0");
+  const remainHours =   String(Math.floor(until / (1000 * 60 * 60 ) % 24)).padStart(2,"0");
+  const remainMinutes = String(Math.floor(until / (1000 * 60 ) % 60)).padStart(2,"0");
+  const remainSeconds = String(Math.floor(until / (1000) % 60)).padStart(2,"0");
+
+  // toLocaleTimeString()을 써보려고 했으나, 
+  //until variable에서 수학적 연산을 거친 후 string 변환이 되어야 해서 포기
+
+  dDayClock.innerText = `${remainDays}일 ${remainHours}시간 ${remainMinutes}분 ${remainSeconds}초`;
 }
-
-inputForm.addEventListener("submit", inputSubmitted);
-
+remainTime();
+setInterval(remainTime, 1000);
